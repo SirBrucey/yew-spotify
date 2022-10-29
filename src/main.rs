7d4bd5c86a71c672ef4570873390fdbbc5fcf358
Stuart Bruce <programming@brucey.me.uk>
2022-10-29 22:32:50 +0100

    Finish layout

--- src/main.rs
+++ src/main.rs
@@ -45,17 +45,23 @@ fn switch(routes: &Routes) -> Html {
 fn app() -> Html {
     html! {
         <BrowserRouter>
-        <div class={classes!("relative", "flex")}>
-            <div>{"Sidebar"}</div>
-            <div class={classes!("flex", "flex-1", "flex-col", "bg-gradient-to-br", "from-black", "to-[#121286]")}>
-                <div>{"Search Bar"}</div>
-                <div class={classes!("px-6", "h-[calc(100vh-72px)]", "overflow-y-scroll", "hide-scrollbar", "flex", "xl:flex-row", "flex-col-reverse")}>
-                    <div class={classes!("flex-1", "h-fit", "pb-40")}>
-                        <Switch <Routes> render={Switch::render(switch)} />
+            <div class={classes!("relative", "flex")}>
+                <div>{"Sidebar"}</div>
+                <div class={classes!("flex", "flex-1", "flex-col", "bg-gradient-to-br", "from-black", "to-[#121286]")}>
+                    <div>{"Search Bar"}</div>
+                    <div class={classes!("px-6", "h-[calc(100vh-72px)]", "overflow-y-scroll", "hide-scrollbar", "flex", "xl:flex-row", "flex-col-reverse")}>
+                        <div class={classes!("flex-1", "h-fit", "pb-40")}>
+                            <Switch <Routes> render={Switch::render(switch)} />
+                        </div>
+                        <div class={classes!("xl:sticky", "relative", "top-0", "h-fit")}>
+                            {"Top Play"}
+                        </div>
                     </div>
                 </div>
+                <div class={classes!("absolute", "h-28", "bottom-0", "left-0", "right-0", "flex", "animate-slideup", "bg-gradient-to-br", "from-white/10", "to-[#2a2a80]", "backdrop-blur-lg", "rounded-t-3xl", "z-10")}>
+                    {"Music Player"}
+                </div>
             </div>
-        </div>
         </BrowserRouter>
     }
 }
